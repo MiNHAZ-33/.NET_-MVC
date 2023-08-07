df231df521aa909056cc0086e2ad3a6a8c805809
MiNHAZ-33 <miminhaz33@gmail.com>
2023-08-07 21:36:03 +0600

    crud operation from fronetend successfull

---
+++ ECommereceMVC/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,90 @@
+﻿using Ecommerce.DataAccess.Repository.IRepository;
+using Ecommerce.Model.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommereceMVC.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            List<Product> objProductsList = _unitOfWork.Product.GetAll().ToList();
+            return View(objProductsList);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Product obj)
+        {
+            if(ModelState.IsValid)
+            {
+                _unitOfWork.Product.Add(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Product created successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id < 1)
+            {
+                return NotFound();
+            }
+            Product? productFromDb = _unitOfWork.Product.Get(u =>  u.Id == id);
+            if(productFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(productFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Product obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Update(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Edited product successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if(id == null || id < 1)
+            {
+                return BadRequest();
+            }
+            Product? product = _unitOfWork.Product.Get(u => u.Id==id);
+            if(product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int? id)
+        {
+            Product? product = _unitOfWork.Product.Get(u =>u.Id==id);
+            if( product == null)
+            {
+                return NotFound();
+            }
+            _unitOfWork.Product.Remove(product);
+            _unitOfWork.Save();
+            TempData["success"] = "Product successfully deleted from the databaase";
+            return RedirectToAction("Index");
+        }
+    }
+}
--- Ecommerce.DataAccess/Repository/ProductRepository.cs
+++ Ecommerce.DataAccess/Repository/ProductRepository.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Ecommerce.DataAccess.Data;
+using Ecommerce.DataAccess.Repository.IRepository;
+using Ecommerce.Model.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,16 @@ using System.Threading.Tasks;
 
 namespace Ecommerce.DataAccess.Repository
 {
-    internal class ProductRepository
+    public class ProductRepository : Repository<Product>, IProductRepository
     {
+        private ApplicationDbContext _db;
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void Update(Product obj)
+        {
+            _db.Products.Update(obj);
+        }
     }
 }
