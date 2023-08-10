98bf80acc8a6fe5acb23c0bf93018b86d5ae79a8
MiNHAZ-33 <miminhaz33@gmail.com>
2023-08-10 22:23:34 +0600

    image uploading from create product

--- ECommereceMVC/Areas/Admin/Controllers/ProductController.cs
+++ ECommereceMVC/Areas/Admin/Controllers/ProductController.cs
@@ -10,9 +10,12 @@ namespace ECommereceMVC.Areas.Admin.Controllers
     public class ProductController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
-        public ProductController(IUnitOfWork unitOfWork)
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
         {
             _unitOfWork = unitOfWork;
+            _webHostEnvironment = webHostEnvironment;
+
         }
         public IActionResult Index()
         {
@@ -51,6 +54,19 @@ namespace ECommereceMVC.Areas.Admin.Controllers
         {
             if(ModelState.IsValid)
             {
+                string wwwRootPath = _webHostEnvironment.WebRootPath;
+                if(file != null)
+                {
+                    string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+                    string productPath = Path.Combine(wwwRootPath, @"images\products");
+
+                    using (var fileStream = new FileStream(Path.Combine(productPath, fileName), FileMode.Create))
+                    {
+                        file.CopyTo(fileStream);
+                    }
+                    obj.Product.ImageUrl = @"\images\products" + fileName;
+                }
+
                 _unitOfWork.Product.Add(obj.Product);
                 _unitOfWork.Save();
                 TempData["success"] = "Product created successfully";
