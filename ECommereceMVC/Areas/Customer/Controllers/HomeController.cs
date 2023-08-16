02b6a005368f785f79f31c674451e02edaef8c40
MiNHAZ-33 <miminhaz33@gmail.com>
2023-08-16 21:31:00 +0600

    homepage design completed

--- ECommereceMVC/Areas/Customer/Controllers/HomeController.cs
+++ ECommereceMVC/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿
+using Ecommerce.DataAccess.Repository.IRepository;
 using Ecommerce.Model.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -9,15 +10,18 @@ namespace ECommereceMVC.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> products = _unitOfWork.Product.GetAll(includeProperties: "Category");
+            return View(products);
         }
 
         public IActionResult Privacy()
