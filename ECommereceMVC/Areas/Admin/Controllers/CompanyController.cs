c9147044f7aaa0e4d3ed4bd733fcff4eb8a0abc0
MiNHAZ-33 <miminhaz33@gmail.com>
2023-08-26 16:50:30 +0600

    added company controller

---
+++ ECommereceMVC/Areas/Admin/Controllers/CompanyController.cs
@@ -0,0 +1,72 @@
+﻿using Ecommerce.DataAccess.Repository.IRepository;
+using Ecommerce.Model.Models;
+using Ecommerce.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommereceMVC.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = SD.Role_Admin)]
+    public class CompanyController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public CompanyController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            List<Company> companies = _unitOfWork.Company.GetAll().ToList();
+            return View(companies);
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return View(new Company());
+            }
+            else
+            {
+                Company comapny = _unitOfWork.Company.Get(u=>u.Id == id);
+                return View(comapny);
+            }
+        }
+        [HttpPost]
+        public IActionResult Upsert(Company companyObj)
+        {
+            if(ModelState.IsValid)
+            {
+                if(companyObj.Id == 0)
+                {
+                    _unitOfWork.Company.Add(companyObj);
+                }
+                else
+                {
+                    _unitOfWork.Company.Update(companyObj);
+                }
+                _unitOfWork.Save();
+                TempData["success"] = "Company created successfully";
+                return RedirectToAction("Index");
+            } else
+            {
+                return View(companyObj);
+            }
+        }
+        [HttpDelete]
+        public IActionResult Delete(int? id)
+        {
+            Company? company = _unitOfWork.Company.Get(u=>u.Id==id);
+            if(company == null)
+            {
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+            _unitOfWork.Company.Remove(company);
+            _unitOfWork.Save();
+            return Json(new { success = true, message = "Delete successfully" });
+
+        }
+
+    }
+}
